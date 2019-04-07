cc8f93e06062436494f243ba39c1556ffb755311
Vinoth Kumar <3402812+vinothsparrow@users.noreply.github.com>
2019-04-07 20:38:16 +0530

    Update hello.js

--- functions/hello.js
+++ functions/hello.js
@@ -2,14 +2,18 @@ var http = require('http');
 
 exports.handler = function(event, context, callback) {
     http.get({
-            hostname: 'www.espncricinfo.com',
+            hostname: 'ipinfo.io',
             port: 80,
             path: '/',
             agent: false  // Create a new agent just for this one request
         }, (res) => {
+            res.setEncoding('utf8');
+            res.on('data', function(chunk){
+                console.log(chunk);
+            });
             callback(null, {
-            statusCode: 200,
-            body: res
-        });
+                statusCode: 200,
+                body: "Hello"
+            });
     });
 }
