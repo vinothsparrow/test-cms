6ae0777bb6f3abcc0ea9a3f4db0a58833951c06f
Vinoth Kumar <3402812+vinothsparrow@users.noreply.github.com>
2019-04-07 20:30:08 +0530

    Update hello.js

--- functions/hello.js
+++ functions/hello.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 exports.handler = function(event, context, callback) {
     http.get({
-            hostname: 'localhost',
+            hostname: 'metadata.google.internal',
             port: 80,
             path: '/',
             agent: false  // Create a new agent just for this one request
